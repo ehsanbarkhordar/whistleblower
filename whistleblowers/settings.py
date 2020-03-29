16162d8560e0ba86e30f1627da75e6578ebc4576
ehsan <dr.enyacc@gmail.com>
2020-03-29 16:45:51 +0430

    fix(): Fix static.

--- whistleblowers/settings.py
+++ whistleblowers/settings.py
@@ -130,7 +130,7 @@ USE_L10N = True
 USE_TZ = True
 PYDEVD_USE_FRAME_EVAL = "NO"
 
-# Static files (CSS, JavaScript, Images)
+# Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
